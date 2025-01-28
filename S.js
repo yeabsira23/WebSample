ba7e06f9a8efec9b3b2957ff7346eb9b67671e93
yeabsira23 <abbytesfamichael@gmail.com>
2025-01-28 22:48:49 +0300

    Add files via upload

---
+++ S.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+})
